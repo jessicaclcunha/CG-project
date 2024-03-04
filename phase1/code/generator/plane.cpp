e8c3f5a4ad2a003d8cca28da30a719f116a39d9b
Martim Redondo <116294262+100664@users.noreply.github.com>
2024-03-04 00:49:55 +0000

    Update plane.cpp

--- phase1/code/generator/plane.cpp
+++ phase1/code/generator/plane.cpp
@@ -31,6 +31,12 @@ FIGURE generate_plane_XZ(int length, int divisions, float h, int baixo) { //o h 
     for (int linha = 0; linha < divisions; linha++){ //itera sobre o eixo do Z (neste caso)
         for (int coluna = 0; coluna < divisions; coluna++){ //itera sobre o eixo do X
             // Primeiro triângulo do quadrado
+
+            //criar o ponto 1, como é que isto funciona? (Xs[0] + coluna * division) é o que? e (h, Zs[0])?
+            // o coluna * division é para nos dar a partição à qual fará parte o ponto, como se fosse um ponto de referência
+            // o h é a altura do plano em relação ao eixo y
+            // o Zs[0] é o ponto inicial do plano, ou seja, está no canto inferior esquerdo
+            //isto vai ser replicado para os outros pontos
             POINT p1 = new_point(Xs[0] + coluna * division, h, Zs[0]); 
             POINT p2 = new_point(Xs[1] + coluna * division, h, Zs[1]);
             POINT p3 = new_point(Xs[2] + coluna * division, h, Zs[2]);
@@ -39,6 +45,142 @@ FIGURE generate_plane_XZ(int length, int divisions, float h, int baixo) { //o h 
             POINT p5 = new_point(Xs[3] + coluna * division, h, Zs[3]);
             POINT p6 = new_point(Xs[2] + coluna * division, h, Zs[2]);
 
+            //print_point(p1);
+            //print_point(p2);
+            //print_point(p3);
+            //print_point(p4);
+            //print_point(p5);
+            //print_point(p6);
+
+            // Inicialize vetores vazios
+            std::vector<POINT> vertices; //"lista" de vertices
+            std::vector<int> indices; //"lista" de indices
+
+            // Adicione os elementos um a um
+            vertices.push_back(p1); //adiciona o ponto 1 à lista de vertices
+            vertices.push_back(p2); //...
+            vertices.push_back(p3); //...
+            vertices.push_back(p4); //...
+            vertices.push_back(p5); //...
+            vertices.push_back(p6); //...
+
+            // Adicione os índices um a um
+            indices.push_back(get_size_vertices(f) - 6); //adiciona o indice do ponto 1 à lista de indices, temos que fazer -6 porque estamos a adicionar 6 pontos, ou seja, queremos que comece no indice 0
+            indices.push_back(get_size_vertices(f) - 5); //...
+            indices.push_back(get_size_vertices(f) - 4); //...
+            indices.push_back(get_size_vertices(f) - 3); //...
+            indices.push_back(get_size_vertices(f) - 2); //...
+            indices.push_back(get_size_vertices(f) - 1); //...
+
+            add_vertexs(f, vertices); //adicionamos oficialmente à figura os vertices
+            add_indexs(f, indices);  //adicionamos oficialmente à figura os indices
+
+        }
+        Zs[0] += division; //aumentamos o Z do ponto inicial
+        Zs[1] += division; //...
+        Zs[2] += division; //...
+        Zs[3] += division; //...
+
+        //isto vai servir para criar a próxima "resma" de pontos
+    }
+
+    return f;
+}
+
+FIGURE generate_plane_XY(int length, int divisions, float h, int baixo) {
+    FIGURE f = create_figure(PLANE, length, divisions);
+
+    float dimension = (float)length / 2;
+    float division = (float)length / divisions;
+    float x1 = -dimension, y1 = -dimension;
+    float x2 = -dimension, y2 = -dimension + division;
+    float x3 = -dimension + division, y3 = -dimension;
+    float x4 = -dimension + division, y4 = -dimension + division;
+
+    float Xs[4] = {x1, x2, x3, x4};
+    float Ys[4] = {y1, y2, y3, y4};
+
+    if (baixo == 1){
+        Xs[1] = x3;
+        Xs[2] = x2;
+        Ys[1] = y3;
+        Ys[2] = y2;
+    }
+
+    for (int linha = 0; linha < divisions; linha++){
+        for (int coluna = 0; coluna < divisions; coluna++){
+            // Primeiro triângulo do quadrado
+            POINT p1 = new_point(Xs[0] + coluna * division, Ys[0], h);
+            POINT p2 = new_point(Xs[1] + coluna * division, Ys[1], h);
+            POINT p3 = new_point(Xs[2] + coluna * division, Ys[2], h);
+            // Segundo triângulo do quadrado
+            POINT p4 = new_point(Xs[1] + coluna * division, Ys[1], h);
+            POINT p5 = new_point(Xs[3] + coluna * division, Ys[3], h);
+            POINT p6 = new_point(Xs[2] + coluna * division, Ys[2], h);
+
+            // Inicialize vetores vazios
+            std::vector<POINT> vertices;
+            std::vector<int> indices;
+
+            // Adicione os elementos um a um
+            vertices.push_back(p1);
+            vertices.push_back(p2);
+            vertices.push_back(p3);
+            vertices.push_back(p4);
+            vertices.push_back(p5);
+            vertices.push_back(p6);
+
+            // Adicione os índices um a um
+            indices.push_back(get_size_vertices(f) - 6);
+            indices.push_back(get_size_vertices(f) - 5);
+            indices.push_back(get_size_vertices(f) - 4);
+            indices.push_back(get_size_vertices(f) - 3);
+            indices.push_back(get_size_vertices(f) - 2);
+            indices.push_back(get_size_vertices(f) - 1);
+
+            add_vertexs(f, vertices);
+            add_indexs(f, indices);
+
+        }
+        Ys[0] += division;
+        Ys[1] += division;
+        Ys[2] += division;
+        Ys[3] += division;
+    }
+
+    return f;
+}
+
+FIGURE generate_plane_YZ(int length, int divisions, float h, int baixo) {
+    FIGURE f = create_figure(PLANE, length, divisions);
+
+    float dimension = (float)length / 2;
+    float division = (float)length / divisions;
+    float y1 = -dimension, z1 = -dimension;
+    float y2 = -dimension, z2 = -dimension + division;
+    float y3 = -dimension + division, z3 = -dimension;
+    float y4 = -dimension + division, z4 = -dimension + division;
+
+    float Ys[4] = {y1, y2, y3, y4};
+    float Zs[4] = {z1, z2, z3, z4};
+
+    if (baixo == 1){
+        Ys[1] = y3;
+        Ys[2] = y2;
+        Zs[1] = z3;
+        Zs[2] = z2;
+    }
+
+    for (int linha = 0; linha < divisions; linha++){
+        for (int coluna = 0; coluna < divisions; coluna++){
+            // Primeiro triângulo do quadrado
+            POINT p1 = new_point(h, Ys[0] + coluna * division, Zs[0]);
+            POINT p2 = new_point(h, Ys[1] + coluna * division, Zs[1]);
+            POINT p3 = new_point(h, Ys[2] + coluna * division, Zs[2]);
+            // Segundo triângulo do quadrado
+            POINT p4 = new_point(h, Ys[1] + coluna * division, Zs[1]);
+            POINT p5 = new_point(h, Ys[3] + coluna * division, Zs[3]);
+            POINT p6 = new_point(h, Ys[2] + coluna * division, Zs[2]);
 
             // Inicialize vetores vazios
             std::vector<POINT> vertices;
@@ -68,133 +210,6 @@ FIGURE generate_plane_XZ(int length, int divisions, float h, int baixo) { //o h 
         Zs[1] += division;
         Zs[2] += division;
         Zs[3] += division;
-    }
-
-    return f;
-}
-
-FIGURE generate_plane_XY(int length, int divisions, float h, int baixo) {
-    FIGURE f = create_figure(PLANE, length, divisions);
-
-    float dimension = (float)length / 2;
-    float division = (float)length / divisions;
-    float x1 = -dimension, y1 = -dimension,
-          x2 = -dimension, y2 = -dimension + division,
-          x3 = -dimension + division, y3 = -dimension,
-          x4 = -dimension + division, y4 = -dimension + division;
-
-    float arrx[4] = {x1, x2, x3, x4};
-    float arry[4] = {y1, y2, y3, y4};
-
-    if (baixo == 1){
-        arrx[1] = x3;
-        arrx[2] = x2;
-        arry[1] = y3;
-        arry[2] = y2;
-    }
-
-    for (int linha = 0; linha < divisions; linha++){
-        for (int coluna = 0; coluna < divisions; coluna++){
-            // Primeiro triângulo do quadrado
-            POINT p1 = new_point(arrx[0] + coluna * division, arry[0], h);
-            POINT p2 = new_point(arrx[1] + coluna * division, arry[1], h);
-            POINT p3 = new_point(arrx[2] + coluna * division, arry[2], h);
-            // Segundo triângulo do quadrado
-            POINT p4 = new_point(arrx[1] + coluna * division, arry[1], h);
-            POINT p5 = new_point(arrx[3] + coluna * division, arry[3], h);
-            POINT p6 = new_point(arrx[2] + coluna * division, arry[2], h);
-
-            // Inicialize vetores vazios
-            std::vector<POINT> vertices;
-            std::vector<int> indices;
-
-            // Adicione os elementos um a um
-            vertices.push_back(p1);
-            vertices.push_back(p2);
-            vertices.push_back(p3);
-            vertices.push_back(p4);
-            vertices.push_back(p5);
-            vertices.push_back(p6);
-
-            // Adicione os índices um a um
-            indices.push_back(get_size_vertices(f) - 6);
-            indices.push_back(get_size_vertices(f) - 5);
-            indices.push_back(get_size_vertices(f) - 4);
-            indices.push_back(get_size_vertices(f) - 3);
-            indices.push_back(get_size_vertices(f) - 2);
-            indices.push_back(get_size_vertices(f) - 1);
-
-            add_vertexs(f, vertices);
-            add_indexs(f, indices);
-
-        }
-        arry[0] += division;
-        arry[1] += division;
-        arry[2] += division;
-        arry[3] += division;
-    }
-
-    return f;
-}
-
-FIGURE generate_plane_YZ(int length, int divisions, float h, int baixo) {
-    FIGURE f = create_figure(PLANE, length, divisions);
-
-    float dimension = (float)length / 2, div_side = (float)length / divisions;
-    float y1 = -dimension, z1 = -dimension,
-          y2 = -dimension, z2 = -dimension + div_side,
-          y3 = -dimension + div_side, z3 = -dimension,
-          y4 = -dimension + div_side, z4 = -dimension + div_side;
-
-    float arry[4] = {y1, y2, y3, y4};
-    float arrz[4] = {z1, z2, z3, z4};
-
-    if (baixo == 1){
-        arry[1] = y3;
-        arry[2] = y2;
-        arrz[1] = z3;
-        arrz[2] = z2;
-    }
-
-    for (int linha = 0; linha < divisions; linha++){
-        for (int coluna = 0; coluna < divisions; coluna++){
-            // Primeiro triângulo do quadrado
-            POINT p1 = new_point(h, arry[0] + coluna * div_side, arrz[0]);
-            POINT p2 = new_point(h, arry[1] + coluna * div_side, arrz[1]);
-            POINT p3 = new_point(h, arry[2] + coluna * div_side, arrz[2]);
-            // Segundo triângulo do quadrado
-            POINT p4 = new_point(h, arry[1] + coluna * div_side, arrz[1]);
-            POINT p5 = new_point(h, arry[3] + coluna * div_side, arrz[3]);
-            POINT p6 = new_point(h, arry[2] + coluna * div_side, arrz[2]);
-
-            // Inicialize vetores vazios
-            std::vector<POINT> vertices;
-            std::vector<int> indices;
-
-            // Adicione os elementos um a um
-            vertices.push_back(p1);
-            vertices.push_back(p2);
-            vertices.push_back(p3);
-            vertices.push_back(p4);
-            vertices.push_back(p5);
-            vertices.push_back(p6);
-
-            // Adicione os índices um a um
-            indices.push_back(get_size_vertices(f) - 6);
-            indices.push_back(get_size_vertices(f) - 5);
-            indices.push_back(get_size_vertices(f) - 4);
-            indices.push_back(get_size_vertices(f) - 3);
-            indices.push_back(get_size_vertices(f) - 2);
-            indices.push_back(get_size_vertices(f) - 1);
-
-            add_vertexs(f, vertices);
-            add_indexs(f, indices);
-
-        }
-        arrz[0] += div_side;
-        arrz[1] += div_side;
-        arrz[2] += div_side;
-        arrz[3] += div_side;
     }
 
     return f;
